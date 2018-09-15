673e443d90b9582369ebff0b001f0c07fb359a4d
Christer Ljung <cljung@cljungmacbookr12.local>
2018-09-15 18:45:27 +0200

    updated to Azure CLI 2.0

--- change-lgw-ipaddr.sh
+++ change-lgw-ipaddr.sh
@@ -34,16 +34,16 @@ fi
   
 ipaddr=$(nslookup $endpointname | awk '/^Address: / { print $2 }')  
 
-lgwrule=$(azure network local-gateway show -g $rgname -n $lgwname --json)
+#lgwrule=$(az network local-gateway show -g $rgname -n $lgwname --json)
+gwipaddr=$(az network local-gateway show -g $rgname -n $lgwname --query "gatewayIpAddress" | sed "s/\"//g")
 
-if [ ${#lgwrule} -gt 2 ]
+if [ ${#gwipaddr} -gt 2 ]
 then
-  gwipaddr=$(echo $lgwrule | jq '.gatewayIpAddress' | sed "s/\"//g")
   echo "$endpointname is currently $gwipaddr"
   if [ "$ipaddr" != "$gwipaddr" ] 
   then
     echo "Updating to $ipaddr..."
-    azure network local-gateway set -g $rgname -n $lgwname -i $ipaddr 
+    az network local-gateway update -g $rgname -n $lgwname --gateway-ip-address $ipaddr 
   else
     echo "nslookup gives same ip - doing nothing"
   fi
